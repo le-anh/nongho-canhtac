0679a64745f2f15436b2e077071929c89c4aab95
tkhaipro <tkhaipro>
2020-03-16 21:19:20 +0700

    add donhang & donhang_chitiet

--- app/DonHang_ChiTiet.php
+++ app/DonHang_ChiTiet.php
@@ -11,5 +11,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class DonHang_ChiTiet extends Model
 {
-	//
+	protected $table = 'donhang_chitiet';
+
+	public function donhang()
+	{
+		return $this->belongsTo('App\DonHang', 'donhang_id');
+	}
+	public function vattu()
+	{
+		return $this->belongsTo('App\VatTu', 'vattu_id');
+	}
 }
